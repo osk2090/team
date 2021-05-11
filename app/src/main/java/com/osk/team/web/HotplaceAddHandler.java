af230d7a762cbb2ac6687a48cdcc5ab694e638ce
philiplee25 <yplunap25@gmail.com>
2021-05-11 09:16:00 +0900

    핫플레이스 추가

--- app/src/main/java/com/osk/team/web/HotplaceAddHandler.java
+++ app/src/main/java/com/osk/team/web/HotplaceAddHandler.java
@@ -1,80 +1,80 @@
 package com.osk.team.web;
 
-import com.osk.team.domain.Hotplace;
-import com.osk.team.service.HotplaceService;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.sql.Date;
+import com.osk.team.domain.Hotplace;
+import com.osk.team.service.HotplaceService;
 
 @SuppressWarnings("serial")
 @WebServlet("/hotplace/add")
 public class HotplaceAddHandler extends HttpServlet {
 
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
 
-        HotplaceService clubService = (HotplaceService) request.getServletContext().getAttribute("hotplaceService");
+    HotplaceService clubService =
+        (HotplaceService) request.getServletContext().getAttribute("hotplaceService");
 
-        Hotplace h = new Hotplace();
+    Hotplace h = new Hotplace();
 
-        request.setCharacterEncoding("UTF-8");
-        //    private int no;//게시판 번호
-        //    private String title;//게시판 제목
-        //    private String content;//게시판 내용
-        //    private Date date;//등록 날짜
-        //    private int count;//조회수
-        //    private String address;//주소
-        //    private String photo;//사진
-
-
-        h.setAddress(request.getParameter("haddress"));
-        h.setTitle(request.getParameter("htitle"));
-        h.setContent(request.getParameter("hcontent"));
-        h.setDate(Date.valueOf(request.getParameter("hdate")));
+    request.setCharacterEncoding("UTF-8");
+    // private int no;//게시판 번호
+    // private String title;//게시판 제목
+    // private String content;//게시판 내용
+    // private Date date;//등록 날짜
+    // private int count;//조회수
+    // private String photo;//사진
 
 
+    h.setAddress(request.getParameter("address"));
+    h.setTitle(request.getParameter("title"));
+    h.setContent(request.getParameter("content"));
+    h.setDate(Date.valueOf(request.getParameter("date")));
 
-        HttpServletRequest httpRequest = request;
-        int loginUser = (int) httpRequest.getSession().getAttribute("loginUser");//회원번호로 받기
-        h.setNo(loginUser);
 
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
 
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>클럽 등록</title>");
+    HttpServletRequest httpRequest = request;
+    int loginUser = (int) httpRequest.getSession().getAttribute("loginUser");// 회원번호로 받기
+    h.setNo(loginUser);
 
-        try {
-//            HotplaceService.add(h);
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
 
-            out.println("<meta http-equiv='Refresh' content='1;url=list'>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>클럽 등록</h1>");
-            out.println("<p>클럽 등록했습니다.</p>");
-        } catch (Exception e) {
-            StringWriter strWriter = new StringWriter();
-            PrintWriter printWriter = new PrintWriter(strWriter);
-            e.printStackTrace(printWriter);
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>핫플레이스 등록</title>");
 
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>클럽 등록 오류</h1>");
-            out.printf("<pre>%s</pre>\n", strWriter.toString());
-            out.println("<p><a href='list'>클럽 목록</a></p>");
-        }
+    try {
+      // HotplaceService.add(h);
 
-        out.println("</body>");
-        out.println("</html>");
+      out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+      out.println("</head>");
+      out.println("<body>");
+      out.println("<h1>핫플레이스 등록</h1>");
+      out.println("<p>핫플레이스 등록했습니다.</p>");
+    } catch (Exception e) {
+      StringWriter strWriter = new StringWriter();
+      PrintWriter printWriter = new PrintWriter(strWriter);
+      e.printStackTrace(printWriter);
+
+      out.println("</head>");
+      out.println("<body>");
+      out.println("<h1>핫플레이스 등록 오류</h1>");
+      out.printf("<pre>%s</pre>\n", strWriter.toString());
+      out.println("<p><a href='list'>핫플레이스 목록</a></p>");
     }
+
+    out.println("</body>");
+    out.println("</html>");
+  }
 }
--- app/src/main/java/com/osk/team/web/HotplaceListHandler.java
+++ app/src/main/java/com/osk/team/web/HotplaceListHandler.java
@@ -1,4 +1,82 @@
 package com.osk.team.web;
 
-public class HotplaceListHandler {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.osk.team.domain.Hotplace;
+import com.osk.team.service.HotplaceService;
+
+@SuppressWarnings("serial")
+@WebServlet("/hotplace/list")
+public class HotplaceListHandler extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    HotplaceService hotplaceService =
+        (HotplaceService) request.getServletContext().getAttribute("hotplaceService");
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>핫플레이스 목록</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>핫플레이스 목록</h1>");
+
+    out.println("<p><a href='form.html'>새 글</a></p>");
+
+    try {
+      List<Hotplace> hotplaces = hotplaceService.listAll();
+
+      out.println("<table border='1'>");
+      out.println("<thead>");
+      out.println("<tr>");
+      out.println("<th>번호</th> <th>제목</th> <th>작성자</th> <th>등록일</th> <th>조회수</th>");
+      out.println("</tr>");
+      out.println("</thead>");
+      out.println("<tbody>");
+
+      for (Hotplace h : hotplaces) {
+        out.printf(
+            "<tr>" + " <td>%d</td>" + " <td><a href='detail?no=%1$d'>%s</a></td>" + " <td>%s</td>"
+                + " <td>%s</td>" + " <td>%d</td> </tr>\n",
+
+            h.getNo()
+        // b.getBtitle(),
+        // b.getWriter().getMname(),
+        // b.getBdate(),
+        // b.getBcount()
+        );
+      }
+      out.println("</tbody>");
+      out.println("</table>");
+
+      out.println("<form action='search' method='get'>");
+      out.println("<input type='text' name='keyword'>");
+      out.println("<button>검색</button>");
+      out.println("</form>");
+
+    } catch (Exception e) {
+      // 상세 오류 내용을 StringWriter로 출력한다.
+      StringWriter strWriter = new StringWriter();
+      PrintWriter printWriter = new PrintWriter(strWriter);
+      e.printStackTrace(printWriter);
+
+      // StringWriter 에 들어 있는 출력 내용을 꺼내 클라이언트로 보낸다.
+      out.printf("<pre>%s</pre>\n", strWriter.toString());
+    }
+
+    out.println("</body>");
+    out.println("</html>");
+  }
 }
